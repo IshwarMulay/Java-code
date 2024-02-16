c4491b18999a420b0c0641daec07bd6acca7fffa
IshwarMulay <159440223+IshwarMulay@users.noreply.github.com>
2024-02-16 13:57:13 +0530

    Program to print Long value

---
+++ PrintLongValue.java
@@ -0,0 +1,19 @@
+// This is a simple Java program to demonstrate printing a Long value
+
+
+
+class PrintLongValue
+{
+public static void main(String [] args)
+{
+	// Declaring and initializing a Long variable named 'value' with the value
+	long value=102622;
+
+	System.out.println(value);
+}
+}
+
+/*output:
+ compile time: success
+ Run time: 102622
+ */
